8e15abe32f9578642f23285c2033e959ec00643d
tarunmandhan <mandhantarun@gmail.com>
2022-06-09 23:25:15 +0530

    uploading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <>
+  <h1> Tarun favourite NETFLIX series :-- </h1>
+  
+  <ol>
+  <li> Money heist </li>
+  <li> Sacred games </li>
+  <li> Kota factory </li>
+  <li> Jamtara </li>
+  <li> Lost in space </li>
+  </ol>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </>,document.getElementById("root"));
+  
